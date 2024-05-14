2573fd25175202997687b8b3578c0bcd826bd622
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-05-14 15:02:22 -0300

    Add liquidity pool contract

---
+++ contracts/liquidity-pool/src/lib.rs
@@ -0,0 +1,14 @@
+#![no_std]
+
+mod contract;
+mod event;
+mod interface;
+mod storage;
+mod types;
+
+#[cfg(test)]
+mod test;
+
+pub mod token {
+    soroban_sdk::contractimport!(file = "../../target/wasm32-unknown-unknown/release/token.wasm");
+}
