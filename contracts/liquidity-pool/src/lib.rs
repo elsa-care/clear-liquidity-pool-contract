4d87c24c00d904b0c1c36852e278b12e09a23add
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-05-28 11:01:36 -0300

    Add lenders in contract

--- contracts/liquidity-pool/src/lib.rs
+++ contracts/liquidity-pool/src/lib.rs
@@ -1,17 +1,23 @@
 #![no_std]
 
+mod interface;
+mod storage;
 mod testutils;
 mod types;
 
-use crate::types::DataKey;
+use crate::{
+    interface::LiquidityPoolTrait,
+    storage::{get_admin, get_all_lenders},
+    types::DataKey,
+};
 use soroban_sdk::{contract, contractimpl, Address, Env};
 
 #[contract]
 pub struct LiquidityPoolContract;
 
 #[contractimpl]
-impl LiquidityPoolContract {
-    pub fn initialize(env: Env, admin: Address, token: Address) {
+impl LiquidityPoolTrait for LiquidityPoolContract {
+    fn initialize(env: Env, admin: Address, token: Address) {
         assert!(
             !env.storage().persistent().has(&DataKey::Admin),
             "contract already initialized with an admin"
@@ -26,11 +32,41 @@ impl LiquidityPoolContract {
             .set(&DataKey::TotalBalance, &0i128);
     }
 
-    pub fn get_total_balance(env: Env) -> i128 {
+    fn get_total_balance(env: Env) -> i128 {
         env.storage()
             .persistent()
             .get::<_, i128>(&DataKey::TotalBalance)
             .unwrap_or(0)
+    }
+
+    fn add_lender(env: Env, admin: Address, lender: Address) {
+        assert_eq!(
+            get_admin(&env),
+            admin,
+            "only the stored admin can add lenders"
+        );
+
+        let mut lenders = get_all_lenders(&env);
+
+        lenders.push_back(lender);
+
+        env.storage().persistent().set(&DataKey::Lenders, &lenders);
+    }
+
+    fn remove_lender(env: Env, admin: Address, lender: Address) {
+        assert_eq!(
+            get_admin(&env),
+            admin,
+            "only the stored admin can add lenders"
+        );
+
+        let mut lenders = get_all_lenders(&env);
+
+        if let Some(index) = lenders.iter().position(|address| address == lender) {
+            lenders.remove(index as u32);
+        }
+
+        env.storage().persistent().set(&DataKey::Lenders, &lenders);
     }
 }
 
