530c097ff34ccdeb2abbd6b4c50c940f2a027fbe
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-05-28 12:00:04 -0300

    Add interface to the contract

---
+++ contracts/liquidity-pool/src/interface.rs
@@ -0,0 +1,11 @@
+use soroban_sdk::{Address, Env};
+
+pub trait LiquidityPoolTrait {
+    fn initialize(env: Env, admin: Address, token: Address);
+
+    fn get_total_balance(env: Env) -> i128;
+
+    fn add_borrower(env: Env, admin: Address, borrower: Address);
+
+    fn remove_borrower(env: Env, admin: Address, lender: Address);
+}
