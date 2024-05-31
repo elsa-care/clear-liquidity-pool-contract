9a5a1963c72000ffab0ca1e2eec007da4e59e038
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-05-31 16:59:52 -0300

    Add tests

--- contracts/liquidity-pool/src/test.rs
+++ contracts/liquidity-pool/src/test.rs
@@ -7,9 +7,7 @@ use soroban_sdk::{testutils::Address as _, Address, Env};
 fn test_initialize() {
     let setup = Setup::new();
 
-    let total_balance = setup.liquid_contract.client().get_total_balance();
-
-    assert_eq!(total_balance, 0i128);
+    assert_eq!(setup.liquid_contract.read_contract_balance(), 0i128);
     assert_eq!(setup.liquid_contract.read_admin(), setup.admin);
     assert_eq!(setup.liquid_contract.read_token(), setup.token.address);
 }
@@ -28,6 +26,229 @@ fn test_already_initialize() {
         .liquid_contract
         .client()
         .initialize(&admin, &token.address);
+}
+
+#[test]
+fn test_balance_with_admin() {
+    let setup = Setup::new();
+
+    let balance = setup.liquid_contract.client().balance(&setup.admin);
+
+    assert_eq!(setup.liquid_contract.read_contract_balance(), balance);
+}
+
+#[test]
+fn test_balance_with_lender() {
+    let setup = Setup::new();
+    let lender = Address::generate(&setup.env);
+
+    setup
+        .liquid_contract
+        .client()
+        .add_lender(&setup.admin, &lender);
+
+    let balance = setup.liquid_contract.client().balance(&lender);
+
+    assert_eq!(setup.liquid_contract.read_lender(&lender), balance);
+}
+
+#[test]
+#[should_panic(expected = "address is not registered")]
+fn test_balance_witout_registered_address() {
+    let setup = Setup::new();
+
+    let unregistered_address = Address::generate(&setup.env);
+
+    setup
+        .liquid_contract
+        .client()
+        .balance(&unregistered_address);
+}
+
+#[test]
+fn test_deposit() {
+    let setup = Setup::new();
+    let lender1 = Address::generate(&setup.env);
+    let lender2 = Address::generate(&setup.env);
+
+    setup
+        .liquid_contract
+        .client()
+        .add_lender(&setup.admin, &lender1);
+
+    setup
+        .liquid_contract
+        .client()
+        .add_lender(&setup.admin, &lender2);
+
+    setup.token_admin.mock_all_auths().mint(&lender1, &4i128);
+    setup.token_admin.mock_all_auths().mint(&lender2, &7i128);
+
+    setup
+        .liquid_contract
+        .client()
+        .mock_all_auths()
+        .deposit(&lender1, &4i128);
+
+    setup
+        .liquid_contract
+        .client()
+        .mock_all_auths()
+        .deposit(&lender2, &7i128);
+
+    assert_eq!(setup.liquid_contract.read_contract_balance(), 11i128);
+    assert_eq!(setup.liquid_contract.read_lender(&lender1), 4i128);
+    assert_eq!(setup.liquid_contract.read_lender(&lender2), 7i128);
+}
+
+#[test]
+#[should_panic(expected = "lender is not registered")]
+fn test_deposit_without_lender() {
+    let setup = Setup::new();
+    let lender = Address::generate(&setup.env);
+
+    setup
+        .liquid_contract
+        .client()
+        .mock_all_auths()
+        .deposit(&lender, &10i128);
+}
+
+#[test]
+#[should_panic(expected = "amount must be positive")]
+fn test_deposit_with_negative_amount() {
+    let setup = Setup::new();
+    let lender = Address::generate(&setup.env);
+
+    setup
+        .liquid_contract
+        .client()
+        .add_lender(&setup.admin, &lender);
+
+    setup
+        .liquid_contract
+        .client()
+        .mock_all_auths()
+        .deposit(&lender, &-10i128);
+}
+
+#[test]
+fn test_withdraw() {
+    let setup = Setup::new();
+    let lender1 = Address::generate(&setup.env);
+    let lender2 = Address::generate(&setup.env);
+
+    setup
+        .liquid_contract
+        .client()
+        .add_lender(&setup.admin, &lender1);
+
+    setup
+        .liquid_contract
+        .client()
+        .add_lender(&setup.admin, &lender2);
+
+    setup.token_admin.mock_all_auths().mint(&lender1, &10i128);
+    setup.token_admin.mock_all_auths().mint(&lender2, &10i128);
+    setup
+        .token_admin
+        .mock_all_auths()
+        .mint(&setup.liquid_contract_id, &20i128);
+
+    setup
+        .liquid_contract
+        .client()
+        .mock_all_auths()
+        .deposit(&lender1, &10i128);
+
+    setup
+        .liquid_contract
+        .client()
+        .mock_all_auths()
+        .deposit(&lender2, &10i128);
+
+    assert_eq!(setup.liquid_contract.read_contract_balance(), 20i128);
+
+    setup
+        .liquid_contract
+        .client()
+        .mock_all_auths()
+        .withdraw(&lender1, &5i128);
+
+    setup
+        .liquid_contract
+        .client()
+        .mock_all_auths()
+        .withdraw(&lender2, &7i128);
+
+    assert_eq!(setup.liquid_contract.read_contract_balance(), 8i128);
+    assert_eq!(setup.liquid_contract.read_lender(&lender1), 5i128);
+    assert_eq!(setup.liquid_contract.read_lender(&lender2), 3i128);
+}
+
+#[test]
+#[should_panic(expected = "lender is not registered")]
+fn test_withdraw_without_lender() {
+    let setup = Setup::new();
+    let lender = Address::generate(&setup.env);
+
+    setup
+        .liquid_contract
+        .client()
+        .mock_all_auths()
+        .withdraw(&lender, &7i128);
+}
+
+#[test]
+#[should_panic(expected = "amount must be positive")]
+fn test_withdraw_negative_amount() {
+    let setup = Setup::new();
+    let lender = Address::generate(&setup.env);
+
+    setup
+        .liquid_contract
+        .client()
+        .add_lender(&setup.admin, &lender);
+
+    setup.token_admin.mock_all_auths().mint(&lender, &7i128);
+
+    setup
+        .liquid_contract
+        .client()
+        .mock_all_auths()
+        .deposit(&lender, &7i128);
+
+    setup
+        .liquid_contract
+        .client()
+        .mock_all_auths()
+        .withdraw(&lender, &-7i128);
+}
+
+#[test]
+#[should_panic(expected = "balance not available for the amount requested")]
+fn test_withdraw_amount_greather_lender_balance() {
+    let setup = Setup::new();
+    let lender = Address::generate(&setup.env);
+
+    setup
+        .liquid_contract
+        .client()
+        .add_lender(&setup.admin, &lender);
+
+    setup.token_admin.mock_all_auths().mint(&lender, &7i128);
+
+    setup
+        .liquid_contract
+        .client()
+        .mock_all_auths()
+        .deposit(&lender, &7i128);
+
+    setup
+        .liquid_contract
+        .client()
+        .mock_all_auths()
+        .withdraw(&lender, &10i128);
 }
 
 #[test]
