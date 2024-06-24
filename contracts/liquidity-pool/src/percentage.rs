4d5b923f38fb1f147b80f1aabf381ebce87fe493
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-06-24 17:54:25 -0300

    Fix compiler errors

--- contracts/liquidity-pool/src/percentage.rs
+++ contracts/liquidity-pool/src/percentage.rs
@@ -7,7 +7,7 @@ fn calculate_percentage(amount: &i128, total_balance: &i128) -> i64 {
 }
 
 fn recalculate_percentage(lender_percentage: i64, old_balance: i128, total_balance: &i128) -> i64 {
-    (lender_percentage * old_balance as i64 / *total_balance as i64) as i64
+    lender_percentage * old_balance as i64 / *total_balance as i64
 }
 
 pub fn process_lender_contribution(
@@ -19,14 +19,14 @@ pub fn process_lender_contribution(
     old_balance: &i128,
 ) -> Map<Address, i64> {
     if lender_contribution.len() == length {
-        let percentage = calculate_percentage(&lender_balance, &total_balance);
+        let percentage = calculate_percentage(lender_balance, total_balance);
         lender_contribution.set(lender.clone(), percentage)
     } else {
         for (address, percentage) in lender_contribution.iter() {
-            let new_percentage = recalculate_percentage(percentage, *old_balance, &total_balance);
+            let new_percentage = recalculate_percentage(percentage, *old_balance, total_balance);
             lender_contribution.set(address.clone(), new_percentage);
         }
-        let percentage = calculate_percentage(&lender_balance, &total_balance);
+        let percentage = calculate_percentage(lender_balance, total_balance);
         lender_contribution.set(lender.clone(), percentage)
     }
     lender_contribution
