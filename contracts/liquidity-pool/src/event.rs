2279eb76a3cf49caee4404cec4f58e1f559017ee
Fede Estigarribia <fede.estigarribia@hotmail.com>
2024-07-11 19:24:27 -0300

    Add events to the contract, when a function executes successfully publishes an event

---
+++ contracts/liquidity-pool/src/event.rs
@@ -0,0 +1,46 @@
+use soroban_sdk::{Address, Env, Symbol};
+
+pub(crate) fn initialize(env: &Env, admin: Address, token: Address) {
+    let topics = (Symbol::new(env, "initialize"), admin, token);
+    env.events().publish(topics, ());
+}
+
+pub(crate) fn deposit(env: &Env, from: Address, amount: i128) {
+    let topics = (Symbol::new(env, "deposit"), from);
+    env.events().publish(topics, amount);
+}
+
+pub(crate) fn withdraw(env: &Env, to: Address, amount: i128) {
+    let topics = (Symbol::new(env, "withdraw"), to);
+    env.events().publish(topics, amount);
+}
+
+pub(crate) fn loan(env: &Env, to: Address, loan_id: u64, amount: i128) {
+    let topics = (Symbol::new(env, "loan"), to, loan_id);
+    env.events().publish(topics, amount);
+}
+
+pub(crate) fn repay_loan(env: &Env, to: Address, loan_id: u64, amount: i128) {
+    let topics = (Symbol::new(env, "repay_loan"), to, loan_id);
+    env.events().publish(topics, amount);
+}
+
+pub(crate) fn add_borrower(env: &Env, admin: Address, borrower: Address) {
+    let topics = (Symbol::new(env, "add_borrower"), admin, borrower);
+    env.events().publish(topics, ());
+}
+
+pub(crate) fn remove_borrower(env: &Env, admin: Address, borrower: Address) {
+    let topics = (Symbol::new(env, "remove_borrower"), admin, borrower);
+    env.events().publish(topics, ());
+}
+
+pub(crate) fn add_lender(env: &Env, admin: Address, lender: Address) {
+    let topics = (Symbol::new(env, "add_lender"), admin, lender);
+    env.events().publish(topics, ());
+}
+
+pub(crate) fn remove_lender(env: &Env, admin: Address, lender: Address) {
+    let topics = (Symbol::new(env, "remove_lender"), admin, lender);
+    env.events().publish(topics, ());
+}
